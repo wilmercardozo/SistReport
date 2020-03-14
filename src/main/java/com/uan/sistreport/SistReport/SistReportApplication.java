75f5ef3ba41418c9a3429d54b2415ccc58d7f9a2
wilmercardozo <wilmer.cardozo@hardtech.co>
2020-03-14 11:22:42 -0500

    commit inicial

---
+++ src/main/java/com/uan/sistreport/SistReport/SistReportApplication.java
@@ -0,0 +1,13 @@
+package com.uan.sistreport.SistReport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistReportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistReportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uan/sistreport/SistReport/SistReportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uan.sistreport.SistReport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistReportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
