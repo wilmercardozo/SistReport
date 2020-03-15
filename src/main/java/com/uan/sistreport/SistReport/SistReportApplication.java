037ca94060fa5c34834466758b486d49680a295d
wilmercardozo <wilmer.cardozo@hardtech.co>
2020-03-14 20:24:34 -0500

    inicio

--- src/main/java/com/uan/sistreport/SistReport/SistReportApplication.java
+++ src/main/java/com/uan/sistreport/SistReport/SistReportApplication.java
@@ -1,13 +1,27 @@
 package com.uan.sistreport.SistReport;
 
+import com.uan.sistreport.SistReport.config.SwaggerConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class SistReportApplication {
+@EnableJpaRepositories("com.uan.sistreport.SistReport")
+@EnableJpaAuditing
+@Import(SwaggerConfiguration.class)
+public class SistReportApplication  implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SistReportApplication.class, args);
 	}
 
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("swagger-ui.html")
+				.addResourceLocations("classpath:/META-INF/resources/");
+	}
 }
